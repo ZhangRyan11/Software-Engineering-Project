070ee33613cc6053cfdb7c99c0bfc718d106f700
ScottVjr <vansises1@newpaltz.edu>
2025-02-08 15:16:36 -0500

    Add check IntegerFactors.java
    Add check for user to put a value over 0, fixed spacing errors

--- IntegerFactors.java
+++ IntegerFactors.java
@@ -8,18 +8,29 @@ public class IntegerFactors {
         // Ask the user for an integer and save it under a variable 
         System.out.print("Enter an integer: ");
         int num = scanner.nextInt();
+        
+        //Check that user gave a value greater than 0
+        if(num =< 0){
+            System.out.print("Enter an int value greater than 0: ");
+            //User fails check, get a new value
+            int num = scanner.nextInt();
+        }
+            
+        //User passes check, get factors 
+        else{
 
-        // Calculate the square of the given number to see how far to check for factors
-        int square = num * num;
+            // Calculate the square of the given number to see how far to check for factors
+            int square = num * num;
 
-        // Print the factors of the number up to its square
-        System.out.println("Factors of " + num + " up to its square (" + square + "): ");
+            // Print the factors of the number up to its square
+            System.out.println("Factors of " + num + " up to its square (" + square + "): ");
 
-        // Go through integers from 1 to the square of the given number
-        for (int i = 1; i <= square; i++) {
-            // Check if i is a factor of the number
-            if (num % i == 0) {
-                System.out.println(i + ", ");
+            // Go through integers from 1 to the square of the given number
+            for (int i = 1; i <= square; i++) {
+                // Check if i is a factor of the number
+                if (num % i == 0) {
+                    System.out.println(i + ", ");
+                }
             }
         }
 
