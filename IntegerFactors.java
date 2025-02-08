ec5f36939080216b960accca4e6d8d3979a43d7b
ScottVjr <vansises1@newpaltz.edu>
2025-02-08 15:01:11 -0500

    Getting INT Factors
    Getting all values up to the square of user input value that the user value is divisible by

---
+++ IntegerFactors.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class IntegerFactors {
+    public static void main(String[] args) {
+        // Create scanner object to read input from user
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user for an integer and save it under a variable 
+        System.out.print("Enter an integer: ");
+        int num = scanner.nextInt();
+
+        // Calculate the square of the given number to see how far to check for factors
+        int square = num * num;
+
+        // Print the factors of the number up to its square
+        System.out.println("Factors of " + num + " up to its square (" + square + "): ");
+
+        // Go through integers from 1 to the square of the given number
+        for (int i = 1; i <= square; i++) {
+            // Check if i is a factor of the number
+            if (num % i == 0) {
+                System.out.println(i + ", ");
+            }
+        }
+
+        // Close the scanner once the loop is done
+        scanner.close();
+    }
+}
