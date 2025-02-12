5791c7000157894c40c5296f4ec964d9584807d2
Jack Lin <lin_jack@myyahoo.com>
2025-02-12 11:25:01 -0500

    removed some code

--- src/api/ComputeEngineAPI.java
+++ src/api/ComputeEngineAPI.java
@@ -1,7 +1,7 @@
 package api;
 import java.util.Scanner;
 
-public class ComputeEngineAPI {
+public class ComputeEngineAPI implements UserInterface{
     public static void main(String[] args) {
         // Create scanner object to read input from user
         Scanner scanner = new Scanner(System.in);
@@ -19,32 +19,7 @@ public class ComputeEngineAPI {
         closeResources(scanner);
     }
 
-    // Method to get a user input int 
-    public static int getUserInput(Scanner scanner) {
-        int num = 0;
-        // Prompt for input until a valid value is entered
-        while (true) {
-            try {
-                // Ask the user for an int and save it under the variable num (override 0)
-                System.out.print("Enter an integer greater than 0: ");
-                num = scanner.nextInt();
-
-                // If the number is 0 or less, throw an exception
-                if (num <= 0) {
-                    throw new IllegalArgumentException("Invalid value. Integer must be greater than 0.");
-                }
-                
-                // When value is valid, break out of the loop
-                break;
-            }   
-            catch (IllegalArgumentException e) {
-                // Catch the exception and print an error message
-                System.out.println(e.getMessage());
-            }
-        }
-        return num; 
-    }
-
+    
     // Method to calculate the square of the number
     public static int calculateSquare(int num) {
         // Calculate the square of the given number to see how far to check for factors
