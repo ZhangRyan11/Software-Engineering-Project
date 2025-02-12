dc0da9b418cf638636f52d5a156ac876b637a870
Jack Lin <lin_jack@myyahoo.com>
2025-02-12 12:19:56 -0500

    Update PrototypeStorage.java

--- src/api/PrototypeStorage.java
+++ src/api/PrototypeStorage.java
@@ -1,19 +1,41 @@
 package api;
 
 import java.io.IOException;
+import api.requests.ComputeRequest;
+import api.requests.ReadRequest;
+import api.responses.ReadResponse;
+import api.requests.WriteRequest;
+import api.responses.WriteResponse;
+
+
+
 
 public class PrototypeStorage {
 
-	public void prototype(PrototypeStorage ds) throws IOException {
-		
-		ComputeRequest computeRequest = new ComputeRequest(null,null,null, null);
-		
-		//Sends the DataStorage a read request with the required information and Receive a response with the input data. 
-		ReadResponse readResponse = ds.read(new ReadRequest(computeRequest));
-		
-		//Sends a write request with the factor result and the destination and Receive some kind of confirmation. 
-		WriteResponse writeResponse = ds.write(new WriteRequest(computeRequest));
-		
-	}
-	
+    // Constructor (if intended)
+    public PrototypeStorage() {
+        // Initialization if needed
+    }
+
+    // Corrected method
+    public void prototype(PrototypeStorage ds) throws IOException {
+        ComputeRequest computeRequest = new ComputeRequest(null, null, null, null);
+
+        // Sends the DataStorage a read request with the required information and receives a response with the input data. 
+        ReadResponse readResponse = ds.read(new ReadRequest(computeRequest));
+
+        // Sends a write request with the factor result and the destination and receives some kind of confirmation. 
+        WriteResponse writeResponse = ds.write(new WriteRequest(computeRequest));
+    }
+
+    // Assuming these methods exist in the class
+    public ReadResponse read(ReadRequest request) {
+        // Implementation here
+        return new ReadResponse();
+    }
+
+    public WriteResponse write(WriteRequest request) {
+        // Implementation here
+        return new WriteResponse();
+    }
 }
