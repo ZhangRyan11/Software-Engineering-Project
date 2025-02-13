c3afa1f587fbd3fc6537de6c8dc0e43f9e753bdd
Jack Lin <lin_jack@myyahoo.com>
2025-02-13 11:55:23 -0500

    Create WrapperMapTest.java

---
+++ test/project/annotations/WrapperMapTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+
+public class WrapperMapTest {
+
+	public void testMap() {
+		String value = "five";
+		int key = 5;
+		WrappedHashMap map = new WrappedHashMap();
+		map.put(key,value);
+		Assertion.assertEquals();
+				
+	}
+	
+}
