3fe76bb7f89afa46acd3ae5c33f104d252b0b246
ZhangRyan11 <zhangr5@newpaltz.edu>
2025-02-13 11:19:34 -0500

    Updated Per Professor

--- test/project/annotations/TestAddingMachine.java
+++ test/project/annotations/TestAddingMachine.java
@@ -1,5 +1,7 @@
 package project.annotations;
 
+import java.util.Random;
+
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -13,13 +15,24 @@ public class TestAddingMachine {
 
 	@Test
 	public void testFuzzyAdding() {
-		//TODO
+		long seed = System.currentTimeMillis();
+		Random random = new Random(seed);	
+		System.out.println("Running with seed:" +seed);
+		AddingMachine toTest = new AddingMachine();
+		
+		for(int i=0;i<100;i++)
+		{
+			int val1 = random.nextInt();
+			int val2 = random.nextInt();
+			checkComputation(val1, val2, toTest);
+		}
 	}
 	
 	private void checkComputation(int a, int b, AddingMachine testAddingMachine) {
 		long actualResult = testAddingMachine.add(a, b);
 		long expectedResult = a;
 		expectedResult += b;
+		System.out.println("adding values"+a+""+b);
 		Assert.assertEquals(expectedResult, actualResult);
 	}
 }
