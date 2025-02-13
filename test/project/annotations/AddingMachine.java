3e2da2167d041020c5bfd6dbddfa1987527976b4
ZhangRyan11 <zhangr5@newpaltz.edu>
2025-02-13 11:09:14 -0500

    InClassExercsie2
    In class

---
+++ test/project/annotations/AddingMachine.java
@@ -0,0 +1,5 @@
+package project.annotations;
+
+public class AddingMachine {
+
+}
---
+++ test/project/annotations/TestAddingMachine.java
@@ -0,0 +1,25 @@
+package exercise2;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class TestAddingMachine {
+
+	@Test
+	public void testSimple() {
+		AddingMachine testAddingMachine = new AddingMachine();
+		checkComputation(2, 3, testAddingMachine);
+	}
+
+	@Test
+	public void testFuzzyAdding() {
+		//TODO
+	}
+	
+	private void checkComputation(int a, int b, AddingMachine testAddingMachine) {
+		long actualResult = testAddingMachine.add(a, b);
+		long expectedResult = a;
+		expectedResult += b;
+		Assert.assertEquals(expectedResult, actualResult);
+	}
+}
