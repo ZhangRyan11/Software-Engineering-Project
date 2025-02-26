45bcff0d9861a85e0e06098e4796628a5710e63f
Jack Lin <lin_jack@myyahoo.com>
2025-02-26 12:00:54 -0500

    Delete check_results.sh

--- check_results.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if grep -q error results.xml; then
-    echo "Checkstyle found an error - look at the logs under Print Results for details"
-    exit 1
-else
-    echo "No errors found!"
-fi
