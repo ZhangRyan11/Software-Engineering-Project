6c7f68ed9c40ad503c3e2d0c00890001953e97c7
Jack Lin <lin_jack@myyahoo.com>
2025-03-06 11:12:26 -0500

    Update ExceptionsIntegrationTest.java

--- test/api/ExceptionsIntegrationTest.java
+++ test/api/ExceptionsIntegrationTest.java
@@ -1,7 +1,7 @@
 package api;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
