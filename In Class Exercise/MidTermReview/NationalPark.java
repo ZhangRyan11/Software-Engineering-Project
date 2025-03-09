cc8e8cbdab472c611b79b7e58b25869a425a0f54
Jack Lin <lin_jack@myyahoo.com>
2025-03-09 16:20:06 -0400

    Update NationalPark.java

---
+++ In Class Exercise/MidTermReview/NationalPark.java
@@ -0,0 +1,27 @@
+public class NationalPark {
+	pulbic void seeeWildlife(Wildlife wildlife) {
+		if (wildlife instaceof Goat) {
+			takePhoto();
+		} else if (wildlife instaof Tiger) {
+			backAwaySlower();
+		}
+	}
+	
+	// code omitted
+}
+
+public class PettingZoo {
+	public void seeWildlife(Wildlife wildlife) {
+		if (wildlife instaceof Goat) {
+			feedAnimal(wildlife);
+		} else if (wild life instaceof Tiger) {
+			cautiouslyPet(wlidlife);
+		}
+	}
+	
+	// code omitted
+}
+
+public interface Wildlife {
+	//marker interface
+}
