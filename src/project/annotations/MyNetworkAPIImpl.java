5ecc82b8d987ac8bfe6ae26b4ec0ba0b1546307b
ZhangRyan11 <zhangr5@newpaltz.edu>
2025-03-29 15:02:26 -0400

    changes

---
+++ src/project/annotations/MyNetworkAPIImpl.java
@@ -0,0 +1,16 @@
+package project.api;
+
+import java.io.File;
+import java.io.PrintWriter;
+
+public class MyNetworkAPIImpl implements MyNetworkAPI {
+    @Override
+    public void doTask(String outputPath) {
+        // Example implementation: write a message to the output file
+        try (PrintWriter writer = new PrintWriter(new File(outputPath))) {
+            writer.println("Task completed successfully!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
