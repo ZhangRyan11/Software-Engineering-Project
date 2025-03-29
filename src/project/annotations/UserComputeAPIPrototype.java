eb46898605691ee5c6e4503a6a6ff1fe8004a94b
Jack Lin <lin_jack@myyahoo.com>
2025-03-29 14:19:06 -0400

    Fixing some checkstyle errors,

--- src/project/annotations/UserComputeAPIPrototype.java
+++ src/project/annotations/UserComputeAPIPrototype.java
@@ -1,11 +1,17 @@
 package project.annotations;
 
 public class UserComputeAPIPrototype {
-    public void setInputSource(String source) { }
+    public void setInputSource(String source) { 
+    	
+    }
     
-    public void setOutputDestination(String destination) { }
+    public void setOutputDestination(String destination) { 
+    	
+    }
     
-    public void setDelimiters(String delimiters) { }
+    public void setDelimiters(String delimiters) { 
+    	
+    }
     
     public String processRequest() { return "Processing request with prototype"; }
     }
