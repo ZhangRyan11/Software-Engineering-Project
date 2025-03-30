04d21ea7dabea50a379376eabd363bad5f6338dd
Jack Lin <lin_jack@myyahoo.com>
2025-03-30 15:59:47 -0400

    Tests setup for coordinators and NetworkAPI is set up

--- test/testHarness/TestUser.java
+++ test/testHarness/TestUser.java
@@ -1,15 +1,16 @@
-package edu.softwareeng.sample;
+package testHarness;
 
 import java.io.File;
 
+import coordinator.NetworkAPI;  // Import the NetworkAPI interface
 
 public class TestUser {
 	
 	// TODO 3: change the type of this variable to the name you're using for your
 	// @NetworkAPI interface; also update the parameter passed to the constructor
-	private final ComputationCoordinator coordinator;
+	private final NetworkAPI coordinator;
 
-	public TestUser(ComputationCoordinator coordinator) {
+	public TestUser(NetworkAPI coordinator) {
 		this.coordinator = coordinator;
 	}
 
@@ -19,6 +20,6 @@ public class TestUser {
 		
 		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
 		// run the compute job specified by inputPath, outputPath, and delimiter
+		coordinator.startComputation(inputPath, outputPath, delimiter);
 	}
-
 }
