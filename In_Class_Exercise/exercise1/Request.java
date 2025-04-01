9785c40bc535a1a4df02c86ad7c3cdb314642c46
Jack Lin <lin_jack@myyahoo.com>
2025-04-01 15:41:38 -0400

    INclass

---
+++ In_Class_Exercise/exercise1/Request.java
@@ -0,0 +1,20 @@
+package exercise1;
+
+public class Request {
+
+	private final int value1;
+	private final int value2;
+	
+	public Request(int value1, int value2) {
+		this.value1 = value1;
+		this.value2 = value2;
+	}
+
+	public int getValue1() {
+		return value1;
+	}
+
+	public int getValue2() {
+		return value2;
+	}
+}
---
+++ In_Class_Exercise/exercise1/Server.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+public class Server {
+
+	public byte[] addValues(byte[] data) {
+		// TODO: set 'request' to a value read from 'data'
+		Request request = null;
+		
+		int result = request.getValue1() + request.getValue2();
+		
+		// TODO: write the result and a success code to a byte array, and return that
+		return null;
+	}
+}
