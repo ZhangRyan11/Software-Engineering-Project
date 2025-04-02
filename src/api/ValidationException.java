cb8d9dacc3967581bc2e7f0064da6d366793041c
Jack Lin <lin_jack@myyahoo.com>
2025-04-02 12:07:53 -0400

    Update ValidationException.java

--- src/api/ValidationException.java
+++ src/api/ValidationException.java
@@ -5,21 +5,11 @@ package api;
  */
 public class ValidationException extends Exception {
     
-    /**
-     * Constructs a new ValidationException with the specified detail message.
-     * 
-     * @param message the detail message
-     */
+
     public ValidationException(String message) {
         super(message);
     }
     
-    /**
-     * Constructs a new ValidationException with the specified detail message and cause.
-     * 
-     * @param message the detail message
-     * @param cause the cause
-     */
     public ValidationException(String message, Throwable cause) {
         super(message, cause);
     }
