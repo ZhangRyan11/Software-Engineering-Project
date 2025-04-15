201a6d89e64834b1891d4baf617222eeec4653a6
Jack Lin <lin_jack@myyahoo.com>
2025-04-15 11:41:59 -0400

    Got DataStorageImpl to register without errors

--- src/api/DataStorageImpl.java
+++ src/api/DataStorageImpl.java
@@ -1,11 +1,7 @@
 package api;
 
-import io.grpc.stub.StreamObserver;
 import datastore.DataStorageGrpc;
-import datastore.DatastoreService.ReadRequest;
-import datastore.DatastoreService.ReadResponse;
-import datastore.DatastoreService.WriteRequest;
-import datastore.DatastoreService.WriteResponse;
+import datastore.Datastoreservice.WriteResponseStatus;
 
 public class DataStorageImpl extends DataStorageGrpc.DataStorageImplBase {
     private final DataStoreImpl dataStore;
@@ -15,29 +11,38 @@ public class DataStorageImpl extends DataStorageGrpc.DataStorageImplBase {
     }
     
     @Override
-    public void read(ReadRequest request, StreamObserver<ReadResponse> responseObserver) {
+    public void read(datastore.Datastoreservice.ReadRequest request, 
+                    io.grpc.stub.StreamObserver<datastore.Datastoreservice.ReadResponse> responseObserver) {
         // Implement read operation using dataStore
-        String key = request.getKey();
+        String key = request.getSource();
         String value = dataStore.read(key);
         
-        ReadResponse response = ReadResponse.newBuilder()
-            .setValue(value)
-            .build();
+        datastore.Datastoreservice.ReadResponse.Builder responseBuilder = 
+            datastore.Datastoreservice.ReadResponse.newBuilder();
             
-        responseObserver.onNext(response);
+        if (value != null) {
+            // Convert string to ASCII values
+            for (char c : value.toCharArray()) {
+                responseBuilder.addData((int)c);
+            }
+        }
+        
+        responseObserver.onNext(responseBuilder.build());
         responseObserver.onCompleted();
     }
     
     @Override
-    public void write(WriteRequest request, StreamObserver<WriteResponse> responseObserver) {
+    public void write(datastore.Datastoreservice.WriteRequest request, 
+                     io.grpc.stub.StreamObserver<datastore.Datastoreservice.WriteResponse> responseObserver) {
         // Implement write operation using dataStore
-        String key = request.getKey();
-        String value = request.getValue();
+        String key = request.getDestination();
+        String value = request.getWriteData();
         boolean success = dataStore.write(key, value);
         
-        WriteResponse response = WriteResponse.newBuilder()
-            .setSuccess(success)
-            .build();
+        datastore.Datastoreservice.WriteResponse response = 
+            datastore.Datastoreservice.WriteResponse.newBuilder()
+                .setStatus(success ? WriteResponseStatus.SUCCESS : WriteResponseStatus.FAILURE)
+                .build();
             
         responseObserver.onNext(response);
         responseObserver.onCompleted();
