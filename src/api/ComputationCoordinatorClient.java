59ecc3393de1cebf34b0e6ca1004b0ea28a2e214
Jack Lin <lin_jack@myyahoo.com>
2025-04-15 13:48:58 -0400

    Pushing in template code for ComputeCoordinatorClient/Server

--- src/api/ComputationCoordinatorClient.java
+++ src/api/ComputationCoordinatorClient.java
@@ -11,6 +11,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import coordinatorservice.ComputationCoordinatorGrpc;
+
 // Client application that connects to the ComputationCoordinator service.
 // Provides methods to submit computation tasks and retrieve results.
 // Includes a command-line interface for user interaction.
