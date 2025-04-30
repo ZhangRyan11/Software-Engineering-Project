1a6d75c68aa1fd5aedb751917e2b6da81d4dc7ce
Jack Lin <lin_jack@myyahoo.com>
2025-04-30 12:41:23 -0400

    Create the storage response class

--- src/api/StorageResponse.java
+++ src/api/StorageResponse.java
@@ -1,12 +1,24 @@
 package api;
 
-public interface StorageResponse {
+import java.util.List;
 
-	public static void addOutput(String data) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	String getDestination();
-
+/**
+ * Response object from storage operations.
+ */
+public class StorageResponse {
+    private final List<Integer> numbers;
+    private final boolean success;
+    
+    public StorageResponse(List<Integer> numbers, boolean success) {
+        this.numbers = numbers;
+        this.success = success;
+    }
+    
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+    
+    public boolean isSuccess() {
+        return success;
+    }
 }
