4ca4dbcb7eebd1f17af0fdd50e7110c47e91e073
Jack Lin <lin_jack@myyahoo.com>
2025-04-30 12:46:56 -0400

    Updated Storage API and Request

--- src/api/StorageAPI.java
+++ src/api/StorageAPI.java
@@ -1,10 +1,23 @@
 package api;
 
-import project.annotations.ProcessAPI;
+import project.annotations.ConceptualAPI;
 
-@ProcessAPI
+@ConceptualAPI
 public interface StorageAPI {
+    /**
+     * Reads data from storage based on the provided request.
+     * 
+     * @param request The storage request containing path and parsing information
+     * @return A response with the parsed data
+     */
     StorageResponse readData(StorageRequest request);
-    void writeData(String destination, String data);
-	String getSource();
+    
+    /**
+     * Writes data to storage at the specified destination.
+     * 
+     * @param destination Path to write the data to
+     * @param data The data to write
+     * @return true if write was successful, false otherwise
+     */
+    boolean writeData(String destination, String data);
 }
--- src/api/StorageRequest.java
+++ src/api/StorageRequest.java
@@ -1,5 +1,22 @@
 package api;
 
-public interface StorageRequest {
-
+/**
+ * Request object for reading data from storage.
+ */
+public class StorageRequest {
+    private final String source;
+    private final String delimiter;
+    
+    public StorageRequest(String source, String delimiter) {
+        this.source = source;
+        this.delimiter = delimiter;
+    }
+    
+    public String getSource() {
+        return source;
+    }
+    
+    public String getDelimiter() {
+        return delimiter;
+    }
 }
