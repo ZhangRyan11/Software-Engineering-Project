68478be5da32891a3ffee51375ed030d1b1fc901
Jack Lin <lin_jack@myyahoo.com>
2025-05-03 14:20:02 -0400

    testuser fixing

--- test/testharness/TestUser.java
+++ test/testharness/TestUser.java
@@ -5,21 +5,18 @@ import java.io.File;
 import coordinator.NetworkAPI;  // Import the NetworkAPI interface
 
 public class TestUser {
-	
-	// TODO 3: change the type of this variable to the name you're using for your
-	// @NetworkAPI interface; also update the parameter passed to the constructor
-	private final NetworkAPI coordinator;
+    
+    private final NetworkAPI coordinator;
 
-	public TestUser(NetworkAPI coordinator) {
-		this.coordinator = coordinator;
-	}
+    public TestUser(NetworkAPI coordinator) {
+        this.coordinator = coordinator;
+    }
 
-	public void run(String outputPath) {
-		char delimiter = ';';
-		String inputPath = "test" + File.separatorChar + "testInputFile.test";
-		
-		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
-		// run the compute job specified by inputPath, outputPath, and delimiter
-		coordinator.startComputation(inputPath, outputPath, delimiter);
-	}
+    public void run(String outputPath) {
+        char delimiter = ';';
+        String inputPath = "test" + File.separatorChar + "testInputFile.test";
+        
+        // Call the appropriate method on the coordinator to run the compute job
+        coordinator.startComputation(inputPath, outputPath, delimiter);
+    }
 }
